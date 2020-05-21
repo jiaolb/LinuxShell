cf93e606087e211c97f356741208c40cf5b54181
jiaolb <604144334@qq.com>
2020-05-21 15:32:35 +0800

    test SSH Key pub

--- python3/example/99mulTable.py
+++ python3/example/99mulTable.py
@@ -1,7 +1,10 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: UTF-8 -*-
 
 for m in range(1,10):
     print ('\n')
     for n in range(1,m+1):
         print ('%d*%d=%d'%(m, n, m*n), end = ' ')
+
+print ("")
+
